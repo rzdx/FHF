f21b63be99500347e80ec516400ff2ebd2d9e392
rzdx <p76021093@mail.ncku.edu.tw>
2013-11-16 21:12:09 +0800

    EFHF_final

--- EFHF_test.m
+++ EFHF_test.m
@@ -6,6 +6,7 @@ end
 [I,PCN]=pio.picrd({'lena_clr','baboon_clr','pepper_clr','lena_clr_64','lena_clr_128','lena_clr_256'});
 nosct=6;
 T=cell(1,6);
+paraT=zeros(15,6,3);
 rn1={'PSNR_Noisy','PSNR_EFHF'};
 rn2={'SSIM_Noisy','SSIM_EFHF'};
 cn=cell(1,nosct);
@@ -16,15 +17,18 @@ PSNR_EFHF=zeros(1,nosct);
 SSIM_nos=zeros(1,nosct);
 SSIM_EFHF=zeros(1,nosct);
 
+allpidx=1;
+
 for bsvct=1:3
     tic;
-    pidx=1;
+    pidx=allpidx;
     impA=10;
     fltrtp=1;
     for ct=1:nosct
         cn{ct}=['imp:',num2str(ct*impA),'%'];
         impimg=nos.imp(I{pidx},ct*impA);
-        fimpimg=fn.EFHFde(bsvct,I{pidx},impimg,fltrtp);
+        [fimpimg,para]=fn.EFHFde(bsvct,I{pidx},impimg,fltrtp);
+        paraT(:,ct,fltrtp)=para;
         PSNR_nos(ct)=fn.PSNR(I{pidx},impimg);
         PSNR_EFHF(ct)=fn.PSNR(I{pidx},fimpimg);
         SSIM_nos(ct)=fn.clrSSIM(I{pidx},impimg);
@@ -39,13 +43,14 @@ for bsvct=1:3
     toc;
     
     tic;
-    pidx=3;
+    pidx=allpidx;
     gauA=5;
     fltrtp=2;
     parfor ct=1:nosct
         cn{ct}=['gau:',num2str(ct*gauA)];
         gauimg=nos.gau(I{pidx},ct*gauA);
-        fgauimg=fn.EFHFde(bsvct,I{pidx},gauimg,fltrtp);
+        [fgauimg,para]=fn.EFHFde(bsvct,I{pidx},gauimg,fltrtp);
+        paraT(:,ct,fltrtp)=para;
         PSNR_nos(ct)=fn.PSNR(I{pidx},gauimg);
         PSNR_EFHF(ct)=fn.PSNR(I{pidx},fgauimg);
         SSIM_nos(ct)=fn.clrSSIM(I{pidx},gauimg);
@@ -59,14 +64,19 @@ for bsvct=1:3
     T{4}={SSIM_mat,rn2,cn};
     toc;
     
+    hyiparaT=zeros(15,6);
+    hygparaT=zeros(15,6);
+    
     tic;
-    pidx=1;
+    pidx=allpidx;
     mixA=5;
-    fltrtp=3;
     parfor ct=1:nosct
         cn{ct}=['mix:',num2str(mixA*ct),'%/',num2str(mixA*ct)];
         miximg=nos.mix(I{pidx},mixA*ct,mixA*ct);
-        fmiximg=fn.EFHFde(bsvct,I{pidx},miximg,fltrtp);
+        [fimiximg,para]=fn.EFHFde(bsvct,I{pidx},miximg,1);
+        hyiparaT(:,ct)=para;
+        [fmiximg,para]=fn.EFHFde(bsvct,I{pidx},fimiximg,2);
+        hygparaT(:,ct)=para;
         PSNR_nos(ct)=fn.PSNR(I{pidx},miximg);
         PSNR_EFHF(ct)=fn.PSNR(I{pidx},fmiximg);
         SSIM_nos(ct)=fn.clrSSIM(I{pidx},miximg);
@@ -88,7 +98,10 @@ for bsvct=1:3
     %         pio.picwtn(O,N);
     %
     %         -----------------------------------------------table_output
-    
-    save(['img_T-',num2str(bsvct),'.mat'], 'T');
+
+    %     save(['img_T-',num2str(bsvct),'.mat'], 'T');
+    %     save(['para_T-',num2str(bsvct),'.mat'], 'paraT');
+    %     save(['hyiparaT-',num2str(bsvct),'.mat'], 'hyiparaT');
+    %     save(['hygparaT-',num2str(bsvct),'.mat'], 'hygparaT');
     tio.tblshow(T);
 end
